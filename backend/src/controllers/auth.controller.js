53ee944da8fcf84af94166cdb6b322913aa06032
Vijay2127 <mvijaykumarpbt@gmail.com>
2025-03-04 08:40:20 +0530

    first commit

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,49 @@
+import { generateToken } from "../lib/utilis.js";
+import User from "../models/user.model.js";
+import bcryptjs from 'bcryptjs';
+export const signup=async (req,res)=>{
+    const {fullname,email,password}=req.body; 
+   try {
+    if(!fullname || !email || !password){
+        return res.status(400).json({message:"All fields are required"})
+    }
+    if(password.length<6){
+       return res.status(400).json({message:"password must be atleast 6 character"})
+    }
+    const user=await User.findOne({email})
+    if(user) return res.status(400).json({message:"Email already exit"});
+    
+    const salt=await bcryptjs.genSalt(10);
+    const hashpassword=await bcryptjs.hash(password,salt);
+    const newUser=new User({
+        fullname,
+        email,
+        password:hashpassword
+    });
+    if(newUser){
+        //Generate JWT token 
+
+       generateToken(newUser._id,res);
+       await newUser.save();
+       res.status(201).json({
+        _id:newUser._id,
+        fullname:newUser.fullname,
+        email:newUser.email,
+        profilePic:newUser.profilePic,
+       });
+
+    }else{
+        res.status(400).json({message:"Invalid User"})
+    }
+   } catch (error) {
+        console.log("Error in signup",error.message);
+        res.status(500).json({message:"Invalid server Error"})
+   }
+};
+
+export const login=(req,res)=>{
+    res.send("this is login")
+}
+export const logout=(req,res)=>{
+    res.send("this is logout")
+}
---
+++ backend/src/models/user.model.js
@@ -0,0 +1,30 @@
+import mongoose, { Model } from 'mongoose';
+
+const userschema=new mongoose.Schema(
+    {
+        email:{
+            type:String,
+            required:true,
+            unique:true
+        },
+        password:{
+            type:String,
+            required:true,
+            minlength:6
+        },
+        fullname:{
+            type:String,
+            required:true,
+            unique:true
+        },
+        profilePic:{
+            type:String,
+            default:""
+        },
+    },
+    {timestamps:true}
+);
+
+const User=mongoose.model("User",userschema);
+
+export default User;
